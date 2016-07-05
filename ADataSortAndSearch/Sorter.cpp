ebeb08a8f2edcf13c5bbbcb25a85f73eac308b56
Wayne <yuckar97@gmail.com>
2016-07-05 12:49:01 +0800

    no message

--- ADataSortAndSearch/Sorter.cpp
+++ ADataSortAndSearch/Sorter.cpp
@@ -9,3 +9,22 @@ CSorter::CSorter()
 CSorter::~CSorter()
 {
 }
+
+void CSorter::bubbleSort(int list[])
+{
+	int length = sizeof(list) / sizeof(int);
+	for (int i = 0; i < length; i++)
+	{
+		for (int index = 0; index < length - i; index++)
+		{
+			if (list[index] > list[index + 1])
+			{
+				//SwappitySwop
+				int temp = list[index];
+				list[index] = list[index + 1];
+				list[index + 1] = temp;
+			}
+		}
+	}
+}
+
--- ADataSortAndSearch/Sorter.h
+++ ADataSortAndSearch/Sorter.h
@@ -4,5 +4,10 @@ class CSorter
 public:
 	CSorter();
 	~CSorter();
+
+	void bubbleSort(int list[]);
+	int selectionSort(int list[], int length);
+	int insertionSort(int list[], int length);
+
 };
 
